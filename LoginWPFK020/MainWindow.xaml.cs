696e49d6e376d5b092bfed29cf4b9216cdf153ba
Ramon Mora <rmora.marquez@gmail.com>
2022-02-15 19:42:02 -0600

    login

--- LoginWPFK020/MainWindow.xaml.cs
+++ LoginWPFK020/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LoginWPFK020.negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,27 @@ namespace LoginWPFK020
     /// </summary>
     public partial class MainWindow : Window
     {
+        private LoginService login = new LoginService();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = usernameTextbox.Text;
+            string password = passwordTextbox.Password;
+
+            if(login.check(username, password) )
+            {
+                //salidaLabel.Content = "Usuario logeado";
+                MessageBox.Show("Exito!", "Usuario logeado"); 
+            }
+            else
+            {
+                MessageBox.Show("Error de login", "Usuario o password incorrectos");
+                //salidaLabel.Content = "Usuario o password incorrectos";
+            }
         }
     }
 }
---
+++ LoginWPFK020/negocio/LoginService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginWPFK020.negocio
+{
+    //POCO  plain old CLR object
+    internal class LoginService 
+    {
+        string usernameValido = "admin";
+        string passwordValido = "nimda";
+
+        public bool check(string username, string password)
+        {
+            if (usernameValido == username &&
+               passwordValido == password)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+       
+    }
+}
